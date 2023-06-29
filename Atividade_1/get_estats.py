437fa7494b17e63d5022740030f13b637e70dbfb
Gabriel Silva de Oliveira <gso@cin.ufpe.br>
2023-06-29 02:19:27 -0300

    correcao final

--- Atividade_1/get_estats.py
+++ Atividade_1/get_estats.py
@@ -7,17 +7,17 @@ def executar():
     ) if 'csv' in csvFile and 'collectSample' not in csvFile]
     print(csvFiles)
 
+    data_estats = {"file_name":[],"mean":[],"std_deviation":[]}
 
     for csvFile in csvFiles:
         df = pd.read_csv(csvFile)
 
-        data_estats = {}
-
-        data_estats["mean"] = df["delta"].mean()
-        data_estats["std_deviation"] = df["delta"].std()
+        data_estats["file_name"].append(csvFile)
+        data_estats["mean"].append(df["delta"].mean())
+        data_estats["std_deviation"].append(df["delta"].std())
         
-        df = pd.DataFrame(data = data_estats,index = [0])
+    df = pd.DataFrame(data = data_estats)
 
-        df.to_csv("dados_atividade.csv",index = false)
+    df.to_csv("dados_atividade.csv",index = False)
 
 executar()
